5a909bdd2773f21ad55fb99a196854b5bb11ece3
liptonzuma <solomonboateng231@gmail.com>
2021-09-27 17:49:15 +0000

    Learnt how to render a template

--- my_app/urls.py
+++ my_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.myfunctioncall,name='index')
+    path('',views.myfunctioncall,name='index'),
+    path('bootstrap',views.bootstrap, name='bootstrap')
+
 ]
--- my_app/views.py
+++ my_app/views.py
@@ -5,3 +5,5 @@ def myfunctioncall(request):
     return HttpResponse('Hello world')
 
 # Create your views here.
+def bootstrap(request):
+    return render(request,'index.html') 
